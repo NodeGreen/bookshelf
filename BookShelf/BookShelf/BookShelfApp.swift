afef8e10af2434084f13aef6a135a3b00224abac
NodeGreen <andrea.prest.endo@gmail.com>
2025-05-24 17:42:59 +0200

    Created project

---
+++ BookShelf/BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Endo on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
